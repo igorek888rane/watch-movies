2ee84dd067324827019066a24981df75c2677c70
Vitaly <poroshenkoe@gmail.com>
2023-04-13 20:24:56 +0300

    test commit

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,31 @@
-import {FC, PropsWithChildren} from 'react';
-import styles from './Layout.module.scss'
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import {LayoutPropsTypes} from "@/components/Layout/types";
-import Meta from "@/components/Layout/Meta";
+import { FC, PropsWithChildren } from 'react';
+import styles from './Layout.module.scss';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import { LayoutPropsTypes } from '@/components/Layout/types';
+import Meta from '@/components/Layout/Meta';
 
-
-const Layout: FC<PropsWithChildren<LayoutPropsTypes>> = ({title, keywords, description, children}) => {
-    return (
-        <>
-           <Meta title={title} keywords={keywords?keywords:''} description={description?description:''}>
-               <Header/>
-               <main className={styles.main}>{children}</main>
-               <Footer/>
-           </Meta>
-        </>
-    );
+const Layout: FC<PropsWithChildren<LayoutPropsTypes>> = ({
+  title,
+  keywords,
+  description,
+  children,
+}) => {
+  return (
+    <>
+      <Meta
+        title={title}
+        keywords={keywords ? keywords : ''}
+        description={description ? description : ''}
+      >
+        <div className={styles.container}>
+          <Header />
+          <main className={styles.main}>{children}</main>
+          <Footer />
+        </div>
+      </Meta>
+    </>
+  );
 };
 
 export default Layout;
