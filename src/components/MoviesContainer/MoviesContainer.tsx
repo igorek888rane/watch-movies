5656e553243cfffe635d4b45b754c60ebd420c2d
Igor Tsuker <igorek888rane@gmail.com>
2023-04-13 19:46:22 +0300

    Dev create

---
+++ src/components/MoviesContainer/MoviesContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  styles from './Movies.module.scss'
+
+const MoviesContainer = () => {
+    return (
+        <div className={styles.movies__container}>
+          Movies
+        </div>
+    );
+};
+
+export default MoviesContainer;
---
+++ src/pages/movies.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout/Layout";
+import MoviesContainer from "@/components/MoviesContainer/MoviesContainer";
+
+
+export default function Movies() {
+    return (
+        <Layout title={'MoviesPage'}>
+            <MoviesContainer/>
+        </Layout>
+    )
+}
