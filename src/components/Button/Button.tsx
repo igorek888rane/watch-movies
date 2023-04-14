13e5c5650e806c24e593ac447d83d23be3332265
Vika Dovgal <vikadovgal@yandex.ru>
2023-04-14 19:43:10 +0400

    компонент Button, начало футера

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren} from 'react';
+import { ButtonPropsTypes } from '@/components/Button/types';
+import styles from './Button.module.scss'
+
+const Button: FC<PropsWithChildren<ButtonPropsTypes>> = ({url, children}) => {
+    return (
+        <a href={url} target="_blank" className={styles.button}>
+            {children}
+        </a>
+    );
+};
+
+export default Button;
