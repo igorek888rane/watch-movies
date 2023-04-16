52e58d9765b106de5b8db0cbf05ed77a00e44bb8
Rafayel <rafayel.mkhitaryan.01@mail.com>
2023-04-17 00:45:49 +0400

    add Carts Movies

--- src/components/MoviesContainer/MoviesContainer.tsx
+++ src/components/MoviesContainer/MoviesContainer.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {PopUp} from '../UI/PopUp/PopUp';
-import Filters from "@/components/Filters/Filters";
+import { PopUp } from '../UI/PopUp/PopUp';
+import Filters from '@/components/Filters/Filters';
+import { Cart } from '../Cart/Cart';
 
 const MoviesContainer = () => {
-    return (
-        <div className={'container'}>
-                <PopUp/>
-                <Filters/>
-        </div>
-    )
+  return (
+    <div>
+      <div className={'container'}>
+        <PopUp />
+        <Filters />
+      </div>
+      <div>
+        <Cart />
+      </div>
+    </div>
+  );
 };
 
 export default MoviesContainer;
